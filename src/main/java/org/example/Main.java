ceb69de4dbf27b9560e7827e19780d2ad0b1cdbc
sho09214 <motohashi.sho@gmail.com>
2025-05-25 21:56:06 +0900

    Add - ABC003-B

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String t = sc.next();
+        String atcoder = "atcoder";
+        boolean result = true;
+
+        for(int i = 0; i < s.length(); i++){
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+
+            if(cs == ct){
+                continue;
+            }
+
+            //どちらかが'@'かつ、もう片方が'atcoder'の文字ならOK
+            if(cs == '@' && atcoder.indexOf(ct) != -1) {
+                continue;
+            }
+
+            if(ct == '@' && atcoder.indexOf(cs) != -1){
+                continue;
+            }
+
+            result = false;
+            break;
+        }
+
+        if(result){
+            System.out.println("You can win");
+        } else {
+            System.out.println("You will lose");
+        }
     }
 }
