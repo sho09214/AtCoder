d9a1ae214b4857531709f1aa966ddc5151081290
sho09214 <motohashi.sho@gmail.com>
2025-05-25 22:12:20 +0900

    Update - ABC003-B

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,94 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+        //自分の回答
+//        Scanner sc = new Scanner(System.in);
+//
+//        String s = sc.next();
+//        String t = sc.next();
+//        String atcoder = "atcoder";
+//        boolean result = true;
+//
+//        for(int i = 0; i < s.length(); i++){
+//            char cs = s.charAt(i);
+//            char ct = t.charAt(i);
+//
+//            if(cs == ct){
+//                continue;
+//            }
+//
+//            //どちらかが'@'かつ、もう片方が'atcoder'の文字ならOK
+//            if(cs == '@' && atcoder.indexOf(ct) != -1) {
+//                continue;
+//            }
+//
+//            if(ct == '@' && atcoder.indexOf(cs) != -1){
+//                continue;
+//            }
+//
+//            result = false;
+//            break;
+//        }
+//
+//        if(result){
+//            System.out.println("You can win");
+//        } else {
+//            System.out.println("You will lose");
+//        }
+
+        //ChatGPTの回答その1
+//        Scanner sc = new Scanner(System.in);
+//
+//        String s = sc.next();
+//        String t = sc.next();
+//        String atcoder = "atcoder";
+//        boolean canWin = true;
+//
+//        for(int i = 0; i < s.length(); i++){
+//            char cs = s.charAt(i);
+//            char ct = t.charAt(i);
+//
+//            if(cs == ct){
+//                continue;
+//            }
+//
+//            boolean csIsAt = cs == '@' && atcoder.indexOf(ct) != -1;
+//            boolean ctIsAt = ct == '@' && atcoder.indexOf(cs) != -1;
+//
+//            if(csIsAt || ctIsAt){
+//                continue;
+//            }
+//
+//            canWin = false;
+//            break;
+//        }
+//        System.out.println(canWin ? "You can win" : "You will lose");
+
+        //CatGPTの回答その2
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
         String t = sc.next();
-        String atcoder = "atcoder";
-        boolean result = true;
+        boolean canWin = true;
+        Set<Character> atcoderSet = new HashSet<>(Arrays.asList('a', 't', 'c', 'o', 'd', 'e', 'r'));
 
         for(int i = 0; i < s.length(); i++){
             char cs = s.charAt(i);
             char ct = t.charAt(i);
 
-            if(cs == ct){
-                continue;
-            }
+            if(cs == ct) continue;
+            if((cs == '@' && atcoderSet.contains(ct)) ||
+                (ct == '@' && atcoderSet.contains(cs))) continue;
 
-            //どちらかが'@'かつ、もう片方が'atcoder'の文字ならOK
-            if(cs == '@' && atcoder.indexOf(ct) != -1) {
-                continue;
-            }
-
-            if(ct == '@' && atcoder.indexOf(cs) != -1){
-                continue;
-            }
-
-            result = false;
+            canWin = false;
             break;
         }
-
-        if(result){
-            System.out.println("You can win");
-        } else {
-            System.out.println("You will lose");
-        }
+        System.out.println(canWin ? "You can win" : "You will lose");
     }
 }
